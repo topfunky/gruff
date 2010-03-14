6eca1cd120fceee10c3724c6ade0bfcfab140ea7
James Coglan <jcoglan@googlemail.com>
2010-03-14 17:35:07 +0000

    Test cases for bar_spacing on SideBars.

--- test/test_side_bar.rb
+++ test/test_side_bar.rb
@@ -8,5 +8,22 @@ class TestGruffSideBar < GruffTestCase
     write_test_file g, 'side_bar.png'    
   end
 
+  def test_bar_spacing
+    g = setup_basic_graph(Gruff::SideBar, 800)
+    g.bar_spacing = 0
+    g.title = "100% spacing between bars"
+    g.write("test/output/side_bar_spacing_full.png")
+
+    g = setup_basic_graph(Gruff::SideBar, 800)
+    g.bar_spacing = 0.5
+    g.title = "50% spacing between bars"
+    g.write("test/output/side_bar_spacing_half.png")
+
+    g = setup_basic_graph(Gruff::SideBar, 800)
+    g.bar_spacing = 1
+    g.title = "0% spacing between bars"
+    g.write("test/output/side_bar_spacing_none.png")
+  end
+
 end
 
