e7dd9b62d891555d14c1282bf4dafc4cdab895c8
BRIMIL01 <brimil01@gmail.com>
2010-12-23 00:11:20 -0800

    Added tests for bar value labels and fix for 0 marker count Added commify to String to make numbers look pretty Added label_format to allow for formatting of value labels

--- lib/gruff/bar.rb
+++ lib/gruff/bar.rb
@@ -92,7 +92,8 @@ protected
         # Subtract half a bar width to center left if requested
         draw_label(label_center - (@center_labels_over_point ? @bar_width / 2.0 : 0.0), point_index)
         if @show_labels_for_bar_values
-          draw_value_label(left_x + (right_x - left_x)/2, conv[0]-30, @norm_data[row_index][3][point_index])
+          val = (@label_formatting || "%.2f") % @norm_data[row_index][3][point_index] 
+          draw_value_label(left_x + (right_x - left_x)/2, conv[0]-30, val.commify)
         end
       end
 
--- test/test_bar.rb
+++ test/test_bar.rb
@@ -444,6 +444,19 @@ class TestGruffBar < GruffTestCase
     g.write("test/output/bar_set_trailing_dots_trunc.png")
   end
 
+  
+  def test_bar_value_labels
+    g = setup_basic_graph
+    g.show_labels_for_bar_values = true
+    g.write("test/output/bar_value_labels.png")
+  end
+  
+  def test_zero_marker_count
+    g = setup_basic_graph
+    g.marker_count = 0
+    g.write("test/output/bar_zero_marker_count.png")
+  end
+  
   protected
 
   def setup_basic_graph(size=800)
