47915b801d57c99c9c61312c9270375fb7675635
Esteban Santana Santana <sesteban@amazon.com>
2015-04-16 20:32:54 -0700

    Fix the placement of the blocks in a side-stacked bar chart.
    Previously the spacing between them would be wrong. Also fixed the case where there was extra padding added if one or more of the column's values was zero.

--- lib/gruff/side_stacked_bar.rb
+++ lib/gruff/side_stacked_bar.rb
@@ -3,17 +3,17 @@ require File.dirname(__FILE__) + '/side_bar'
 require File.dirname(__FILE__) + '/stacked_mixin'
 
 ##
-# New gruff graph type added to enable sideways stacking bar charts 
+# New gruff graph type added to enable sideways stacking bar charts
 # (basically looks like a x/y flip of a standard stacking bar chart)
 #
-# alun.eyre@googlemail.com 
+# alun.eyre@googlemail.com
 
 class Gruff::SideStackedBar < Gruff::SideBar
   include StackedMixin
 
   # Spacing factor applied between bars
   attr_accessor :bar_spacing
-  
+
   def draw
     @has_left_labels = true
     get_maximum_by_stack
@@ -32,6 +32,7 @@ class Gruff::SideStackedBar < Gruff::SideBar
     @d = @d.stroke_opacity 0.0
     height = Array.new(@column_count, 0)
     length = Array.new(@column_count, @graph_left)
+
     padding = (@bar_width * (1 - @bar_spacing)) / 2
     if @show_labels_for_bar_values
       label_values = Array.new
@@ -40,28 +41,34 @@ class Gruff::SideStackedBar < Gruff::SideBar
     @norm_data.each_with_index do |data_row, row_index|
       data_row[DATA_VALUES_INDEX].each_with_index do |data_point, point_index|
 
-    	  ## using the original calcs from the stacked bar chart to get the difference between
-    	  ## part of the bart chart we wish to stack.
-    	  temp1 = @graph_left + (@graph_width -
-                                    data_point * @graph_width - 
-                                    height[point_index]) + 1
-    	  temp2 = @graph_left + @graph_width - height[point_index] - 1
-    	  difference = temp2 - temp1
+        ## using the original calcs from the stacked bar chart to get the difference between
+        ## part of the bart chart we wish to stack.
+        temp1 = @graph_left + (@graph_width - data_point * @graph_width - length[point_index])
+        temp2 = @graph_left + @graph_width - length[point_index]
 
-    	  @d = @d.fill data_row[DATA_COLOR_INDEX]
+        #Calculate the length of our bar, make sure its not negative.
+        difference = temp2 - temp1
+        difference = [difference, 0].max
 
-        left_x = length[point_index] #+ 1
-              left_y = @graph_top + (@bar_width * point_index) + padding
-    	  right_x = left_x + difference
-              right_y = left_y + @bar_width * @bar_spacing
-    	  length[point_index] += difference
-        height[point_index] += (data_point * @graph_width - 2)
-        
+        #Pick up the bar's color from our table's config
+        @d = @d.fill data_row[DATA_COLOR_INDEX]
+
+        #Calculate the four corners of our bar
+        left_x = length[point_index]
+        left_y = @graph_top + (@bar_width * point_index) + padding
+
+        right_x = left_x + difference
+        right_y = left_y + @bar_width * @bar_spacing
+
+        #Adjust the starting points for the next bar
+        length[point_index] += difference
+        height[point_index] += (data_point * @graph_width - 4)
+
         if @show_labels_for_bar_values
           label_values[point_index][:value] += @norm_data[row_index][3][point_index]
           label_values[point_index][:right_x] = right_x
         end
-                
+
         # if a data point is 0 it can result in weird really thing lines
         # that shouldn't even be there being drawn on top of the existing
         # bar - this is bad
@@ -76,14 +83,15 @@ class Gruff::SideStackedBar < Gruff::SideBar
       end
 
     end
+
     if @show_labels_for_bar_values
       label_values.each_with_index do |data, i|
         val = (@label_formatting || "%.2f") % data[:value]
         draw_value_label(data[:right_x]+40, (@graph_top + (((i+1) * @bar_width) - (@bar_width / 2)))-12, val.commify, true)
       end
     end
-    
-    @d.draw(@base_image)    
+
+    @d.draw(@base_image)
   end
 
   def larger_than_max?(data_point, index=0)
@@ -93,5 +101,4 @@ class Gruff::SideStackedBar < Gruff::SideBar
   def max(data_point, index)
     @data.inject(0) {|sum, item| sum + item[DATA_VALUES_INDEX][index]}
   end
-
 end
