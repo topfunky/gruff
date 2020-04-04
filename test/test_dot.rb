89723723218d8333690675e4cd562da6ca4972ba
Watson <watson1978@gmail.com>
2020-04-05 01:57:02 +0900

    Add fonts in order to get rid of warning at running test
    Added fonts that can download from:
    
    * https://fonts.google.com/specimen/Comic+Neue
    * https://fonts.google.com/specimen/Roboto

--- test/test_dot.rb
+++ test/test_dot.rb
@@ -199,7 +199,7 @@ class TestGruffDot < GruffTestCase
   def test_custom_theme
     g = Gruff::Dot.new
     g.title = 'Custom Theme'
-    g.font = File.expand_path('CREABBRG.TTF', ENV['MAGICK_FONT_PATH'])
+    g.font = File.join(fixtures_dir, 'ComicNeue-Regular.ttf')
     g.title_font_size = 60
     g.legend_font_size = 32
     g.marker_font_size = 32
