f2692230a7d0a2fd4b6fee9db319b0ec51267b88
Watson <watson1978@gmail.com>
2020-04-18 01:34:05 +0900

    Test: Do no use ramdom data (#261)
    Because random values make it difficult to detect error.

--- test/test_bullet.rb
+++ test/test_bullet.rb
@@ -18,12 +18,44 @@ class TestGruffBullet < GruffTestCase
     g = Gruff::Bullet.new
     g.title = "Monthly Revenue"
     g.data(*@data_args)
+    g.replace_colors(
+      %w[
+        #0779e4
+        #4cbbb9
+        #77d8d8
+        #2c003e
+        #ffa372
+        #ffbd69
+        #85a392
+        #efa8e4
+        #5a3f11
+        #2b580c
+        #323232
+        #bae5e5
+      ]
+    )
     g.write("test/output/bullet_greyscale.png")
   end
 
   def test_no_options
     g = Gruff::Bullet.new
     g.data(*@data_args)
+    g.replace_colors(
+      %w[
+        #0779e4
+        #4cbbb9
+        #77d8d8
+        #2c003e
+        #ffa372
+        #ffbd69
+        #85a392
+        #efa8e4
+        #5a3f11
+        #2b580c
+        #323232
+        #bae5e5
+      ]
+    )
     g.write("test/output/bullet_no_options.png")
   end
 
