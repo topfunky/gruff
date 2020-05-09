c9ec9ef0581145f12780bad185a34ee2dab5177d
Watson <watson1978@gmail.com>
2020-05-10 01:17:43 +0900

    refactor: Add push/pop In order to no affect other rendering settings (#344)

--- lib/gruff/renderer/rectangle.rb
+++ lib/gruff/renderer/rectangle.rb
@@ -9,9 +9,11 @@ module Gruff
     def render(upper_left_x, upper_left_y, lower_right_x, lower_right_y)
       draw = Renderer.instance.draw
 
+      draw.push
       draw.stroke('transparent')
       draw.fill(@color) if @color
       draw.rectangle(upper_left_x, upper_left_y, lower_right_x, lower_right_y)
+      draw.pop
     end
   end
 end
