f1927afdbc0bc7988957710bfdc317eef1c628b1
Watson <watson1978@gmail.com>
2020-05-12 20:46:07 +0900

    Revert "Disable antialias when drawing marks in the legend (#350)"
    This reverts commit db78a438eabe888162ee7024ac9d4c552cdf0502.

--- lib/gruff/renderer/rectangle.rb
+++ lib/gruff/renderer/rectangle.rb
@@ -4,14 +4,12 @@ module Gruff
   class Renderer::Rectangle
     def initialize(args = {})
       @color = args[:color]
-      @antialias = args[:antialias].nil? ? true : args[:antialias]
     end
 
     def render(upper_left_x, upper_left_y, lower_right_x, lower_right_y)
       draw = Renderer.instance.draw
 
       draw.push
-      draw.stroke_antialias(@antialias)
       draw.stroke('transparent')
       draw.fill(@color) if @color
       draw.rectangle(upper_left_x, upper_left_y, lower_right_x, lower_right_y)
