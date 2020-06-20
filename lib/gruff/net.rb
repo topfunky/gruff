99b95f86dc13d60ea2bbd236f0dc80d3ad9627a2
Watson <watson1978@gmail.com>
2020-06-20 23:09:42 +0900

    Adjust label position in Gruff::Net (#436)

--- lib/gruff/net.rb
+++ lib/gruff/net.rb
@@ -48,8 +48,7 @@ class Gruff::Net < Gruff::Base
 
     return unless data_given?
 
-    stroke_width = @line_width  || clip_value_if_greater_than(@columns / (store.norm_data.first.points.size * 4), 5.0)
-    circle_radius = @dot_radius || clip_value_if_greater_than(@columns / (store.norm_data.first.points.size * 2.5), 5.0)
+    stroke_width = @line_width || clip_value_if_greater_than(@columns / (store.norm_data.first.points.size * 4), 5.0)
 
     store.norm_data.each do |data_row|
       data_row.points.each_with_index do |data_point, index|
@@ -69,7 +68,7 @@ class Gruff::Net < Gruff::Base
 
         Gruff::Renderer::Line.new(color: data_row.color, width: stroke_width).render(start_x, start_y, end_x, end_y)
 
-        Gruff::Renderer::Circle.new(color: data_row.color, width: stroke_width).render(start_x, start_y, start_x - circle_radius, start_y) unless @hide_dots
+        Gruff::Renderer::Circle.new(color: data_row.color, width: stroke_width).render(start_x, start_y, start_x - @circle_radius, start_y) unless @hide_dots
       end
     end
 
@@ -82,8 +81,9 @@ private
     super
 
     @radius = @graph_height / 2.0
+    @circle_radius = @dot_radius || clip_value_if_greater_than(@columns / (store.norm_data.first.points.size * 2.5), 5.0)
     @center_x = @graph_left + (@graph_width / 2.0)
-    @center_y = @graph_top + (@graph_height / 2.0) - 10 # Move graph up a bit
+    @center_y = @graph_top + (@graph_height / 2.0) + 10
   end
 
   # the lines connecting in the center, with the first line vertical
@@ -98,16 +98,15 @@ private
                            .render(@center_x, @center_y, @center_x + Math.sin(rad_pos) * @radius, @center_y - Math.cos(rad_pos) * @radius)
 
       marker_label = @labels[index] ? @labels[index].to_s : '000'
-      draw_label(@center_x, @center_y, rad_pos * 360 / (2 * Math::PI), @radius, marker_label)
+      draw_label(@center_x, @center_y, rad_pos * 360 / (2 * Math::PI), @radius + @circle_radius, marker_label)
     end
   end
 
   def draw_label(center_x, center_y, angle, radius, amount)
-    r_offset = 1.1
     x_offset = center_x # + 15 # The label points need to be tweaked slightly
     y_offset = center_y # + 0  # This one doesn't though
-    x = x_offset + (radius * r_offset * Math.sin(deg2rad(angle)))
-    y = y_offset - (radius * r_offset * Math.cos(deg2rad(angle)))
+    x = x_offset + (radius + LABEL_MARGIN) * Math.sin(deg2rad(angle))
+    y = y_offset - (radius + LABEL_MARGIN) * Math.cos(deg2rad(angle))
 
     # Draw label
     text_renderer = Gruff::Renderer::Text.new(amount, font: @font, size: 20, color: @marker_color, weight: Magick::BoldWeight)
