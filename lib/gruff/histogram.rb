985dad3e54dab4a10cd37ccf15c7faa61b27de28
Watson <watson1978@gmail.com>
2020-06-20 23:52:21 +0900

    Allow to change settings even after entered the data in Gruff::Histogram

--- lib/gruff/histogram.rb
+++ lib/gruff/histogram.rb
@@ -15,6 +15,20 @@ require 'gruff/base'
 #   g.write('histogram.png')
 #
 class Gruff::Histogram < Gruff::Bar
+  # Specifies interpolation between the min and max of the set. Default is +10+.
+  attr_writer :bin_width
+
+  # Specifies minimum value for bin.
+  attr_writer :minimum_bin
+
+  # Specifies maximum value for bin.
+  attr_writer :maximum_bin
+
+  def initialize(*)
+    super
+    @data = []
+  end
+
   def initialize_ivars
     super
     @bin_width = 10
@@ -23,33 +37,20 @@ class Gruff::Histogram < Gruff::Bar
   end
   private :initialize_ivars
 
-  # Specifies interpolation between the min and max of the set. Default is +10+.
-  def bin_width=(width)
-    raise 'bin_width= should be called before set the data.' unless store.empty?
-
-    @bin_width = width
-  end
-
-  # Specifies minimum value for bin.
-  def minimum_bin=(min)
-    raise 'minimum_bin= should be called before set the data.' unless store.empty?
-
-    @minimum_bin = min
-  end
-
-  # Specifies maximum value for bin.
-  def maximum_bin=(max)
-    raise 'maximum_bin= should be called before set the data.' unless store.empty?
-
-    @maximum_bin = max
-  end
-
   def data(name, data_points = [], color = nil)
-    bins, freqs = HistogramArray.new(data_points).histogram(bin_width: @bin_width, min: @minimum_bin, max: @maximum_bin)
-    bins.each_with_index do |bin, index|
-      @labels[index] = bin
+    @data << [name, data_points, color]
+  end
+
+  def draw
+    @data.each do |(name, data_points, color)|
+      bins, freqs = HistogramArray.new(data_points).histogram(bin_width: @bin_width, min: @minimum_bin, max: @maximum_bin)
+      bins.each_with_index do |bin, index|
+        @labels[index] = bin
+      end
+      store.add(name, freqs, color)
     end
-    store.add(name, freqs, color)
+
+    super
   end
 
   # @private
