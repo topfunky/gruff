85b50dcf8248ab936c898f8ad557be0b949fe267
E. Lynette Rayle <elrayle@users.noreply.github.com>
2020-09-14 08:34:43 -0400

    add hide_labels to stacked side bar graphs (#452)

--- test/test_sidestacked_bar.rb
+++ test/test_sidestacked_bar.rb
@@ -12,46 +12,40 @@ class TestGruffSideStackedBar < GruffTestCase
       #[:Philip, [90, 34, 23, 12, 78, 89, 98, 88]],
       #["Arthur", [5, 10, 13, 11, 6, 16, 22, 32]],
     ]
-    @sample_labels = {
-      0 => '5/6',
-      1 => '5/15',
-      2 => '5/24'
-    }
-  end
-
-  def test_bar_graph
-    g = Gruff::SideStackedBar.new
-    g.title = 'Visual Stacked Bar Graph Test'
-    g.labels = {
+    @labels = {
       0 => '5/6',
       1 => '5/15',
       2 => '5/24',
       3 => '5/30'
     }
-    @datasets.each do |data|
-      g.data(data[0], data[1])
-    end
+    @long_labels = {
+      0 => 'September',
+      1 => 'Oct',
+      2 => 'Nov',
+      3 => 'Dec'
+    }
+  end
+
+  def test_bar_graph
+    g = setup_basic_graph
+    g.title = 'Visual Stacked Bar Graph Test'
     g.write('test/output/side_stacked_bar_keynote.png')
     assert_same_image('test/expected/side_stacked_bar_keynote.png', 'test/output/side_stacked_bar_keynote.png')
   end
 
   def test_bar_graph_small
-    g = Gruff::SideStackedBar.new(400)
+    g = setup_basic_graph(400)
     g.title = 'Visual Stacked Bar Graph Test'
-    g.labels = {
-      0 => '5/6',
-      1 => '5/15',
-      2 => '5/24',
-      3 => '5/30'
-    }
-    @datasets.each do |data|
-      g.data(data[0], data[1])
-    end
     g.write('test/output/side_stacked_bar_keynote_small.png')
     assert_same_image('test/expected/side_stacked_bar_keynote_small.png', 'test/output/side_stacked_bar_keynote_small.png')
   end
 
   def test_wide
+    @labels = {
+      0 => '5/6',
+      1 => '5/15',
+      2 => '5/24'
+    }
     g = setup_basic_graph('800x400')
     g.title = 'Wide SSBar'
     g.write('test/output/side_stacked_bar_wide.png')
@@ -59,36 +53,48 @@ class TestGruffSideStackedBar < GruffTestCase
   end
 
   def test_should_space_long_left_labels_appropriately
-    g = Gruff::SideStackedBar.new
+    @labels = @long_labels
+    g = setup_basic_graph
     g.title = 'Stacked Bar Long Label'
-    g.labels = {
-      0 => 'September',
-      1 => 'Oct',
-      2 => 'Nov',
-      3 => 'Dec'
-    }
-    @datasets.each do |data|
-      g.data(data[0], data[1])
-    end
     g.write('test/output/side_stacked_bar_long_label.png')
     assert_same_image('test/expected/side_stacked_bar_long_label.png', 'test/output/side_stacked_bar_long_label.png')
   end
 
   def test_bar_labels
-    g = Gruff::SideStackedBar.new
+    @labels = @long_labels
+    g = setup_basic_graph
     g.title = 'Stacked Bar Long Label'
-    g.labels = {
-      0 => 'September',
-      1 => 'Oct',
-      2 => 'Nov',
-      3 => 'Dec'
-    }
-    @datasets.each do |data|
-      g.data(data[0], data[1])
-    end
     g.show_labels_for_bar_values = true
     g.write('test/output/side_stacked_bar_labels.png')
     assert_same_image('test/expected/side_stacked_bar_labels.png', 'test/output/side_stacked_bar_labels.png')
+  end
+
+  def test_no_labels
+    @labels = @long_labels
+    g = setup_basic_graph(400)
+    g.title = 'No Labels'
+    g.hide_labels = true
+    g.write('test/output/side_stacked_bar_no_labels.png')
+    assert_same_image('test/expected/side_stacked_bar_no_labels.png', 'test/output/side_stacked_bar_no_labels.png')
+  end
+
+  def test_no_line_markers_or_labels
+    @labels = @long_labels
+    g = setup_basic_graph(400)
+    g.title = 'No Line Markers or Labels'
+    g.hide_labels = true
+    g.hide_line_markers = true
+    g.write('test/output/side_stacked_bar_no_line_markers_or_labels.png')
+    assert_same_image('test/expected/side_stacked_bar_no_line_markers_or_labels.png', 'test/output/side_stacked_bar_no_line_markers_or_labels.png')
+  end
+
+  def test_no_line_markers
+    @labels = @long_labels
+    g = setup_basic_graph(400)
+    g.title = 'No Line Markers'
+    g.hide_line_markers = true
+    g.write('test/output/side_stacked_bar_no_line_markers.png')
+    assert_same_image('test/expected/side_stacked_bar_no_line_markers.png', 'test/output/side_stacked_bar_no_line_markers.png')
   end
 
   def test_draw_twice
@@ -105,7 +111,7 @@ protected
   def setup_basic_graph(size = 800)
     g = Gruff::SideStackedBar.new(size)
     g.title = 'My Graph Title'
-    g.labels = @sample_labels
+    g.labels = @labels
     @datasets.each do |data|
       g.data(data[0], data[1])
     end
