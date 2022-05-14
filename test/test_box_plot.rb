41a076570e6faf05e533127294a83c41e5a216a4
Watson <watson1978@gmail.com>
2022-05-14 13:58:45 +0900

    test: Add spacing_factor tests

--- test/test_box_plot.rb
+++ test/test_box_plot.rb
@@ -17,4 +17,18 @@ class TestBoxPlot < GruffTestCase
     g.write('test/output/box_plot.png')
     assert_same_image('test/expected/box_plot.png', 'test/output/box_plot.png')
   end
+
+  def test_box_plot_spacing_factor
+    g = Gruff::BoxPlot.new
+    g.theme_pastel
+    g.title = 'Box Plot spacing_factor'
+    g.data 'A', [2, 3, 5, 6, 8, 10, 11, 15, 17, 20, 28, 29, 33, 34, 45, 46, 49, 61]
+    g.data 'B', [3, 4, 34, 35, 38, 39, 45, 60, 61, 69, 80, 130]
+    g.data 'C', [4, 40, 41, 46, 57, 64, 77, 76, 79, 78, 99, 153]
+    g.y_axis_increment = 20
+    g.spacing_factor = 0.25
+
+    g.write('test/output/box_plot_spacing_factor.png')
+    assert_same_image('test/expected/box_plot_spacing_factor.png', 'test/output/box_plot_spacing_factor.png')
+  end
 end
