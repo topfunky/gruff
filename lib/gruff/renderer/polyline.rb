ba255933e41a4ffc19a4a618b70e63aac55fe40d
Watson <watson1978@gmail.com>
2022-08-11 02:48:39 +0900

    Improve joints in Gruff::Line using polyline method (#607)

--- lib/gruff/renderer/polyline.rb
+++ lib/gruff/renderer/polyline.rb
@@ -3,16 +3,18 @@
 module Gruff
   # @private
   class Renderer::Polyline
-    def initialize(renderer, color:, width:)
+    def initialize(renderer, color:, width: 1.0, linejoin: 'round')
       @renderer = renderer
       @color = color
       @width = width
+      @linejoin = linejoin
     end
 
     def render(points)
       @renderer.draw.push
-      @renderer.draw.stroke(@color)
       @renderer.draw.fill('transparent')
+      @renderer.draw.stroke(@color)
+      @renderer.draw.stroke_linejoin(@linejoin)
       @renderer.draw.stroke_width(@width)
       @renderer.draw.polyline(*points)
       @renderer.draw.pop
