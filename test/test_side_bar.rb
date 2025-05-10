edb5f8da7d89eed6b603bb17313c96436d129d78
Shizuo Fujita <watson1978@gmail.com>
2025-05-11 08:16:01 +0900

    Update tests to check that no exceptions occur with nil data (#649)

--- test/test_side_bar.rb
+++ test/test_side_bar.rb
@@ -246,7 +246,7 @@ class TestGruffSideBar < GruffTestCase
 
   def test_duck_typing
     g = Gruff::SideBar.new
-    g.data :Jimmy, [25, 36, 86, 39, 25, 31, 79, 88], '#113285'
+    g.data :Jimmy, [nil, 36, 86, 39, 25, 31, 79, 88], '#113285'
     g.data :Charles, [80, 54, 67, 54, 68, 70, 90, 95], '#86A697'
     g.data :Julie, [22, 29, 35, 38, 36, 40, 46, 57], '#E03C8A'
     g.data :Jane, [95, 95, 95, 90, 85, 80, 88, 100], '#72636E'
